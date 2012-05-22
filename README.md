bff2a9a855442aa1691d36db459b103afc66408a
Mark Wolfe <mark@wolfe.id.au>
2012-05-22 23:02:42 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to IPSEC Configurator
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpsecConfigurator::Application.config.secret_token = 'b17a5dafbd99685f189faa26709e37f969fe7d9b5c96805f78528569bfbc4d5b3e854b2db887a7c9286e8a4e842c8789985f1c37e164bdf9b2d3385c0e6d65a0'
